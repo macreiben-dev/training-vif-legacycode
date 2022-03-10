cca74405df453480fcf9a8058b4e40ab13d87090
MacReiben <macreiben@gmail.com>
2022-03-10 16:49:20 +0100

    unused test removed.

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,17 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class GildedRoseTest {
-
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
 
     @Test
     void testrun_on_twentyDays() {
