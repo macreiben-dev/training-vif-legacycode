706bb6e6369b1b810cfbbe3fd5c5a443a271dc81
MacReiben <macreiben@gmail.com>
2022-03-10 16:03:03 +0100

    golden master taken, starting to refactor.

---
+++ gilded-rose/src/test/java/com/gildedrose/TexttestFixtureForTesting.java
@@ -0,0 +1,53 @@
+package com.gildedrose;
+
+import java.util.ArrayList;
+
+public class TexttestFixtureForTesting {
+    public static ArrayList<String> main(String[] args) {
+        ArrayList<String> allLogs = new ArrayList<>();
+
+        log("OMGHAI!", allLogs);
+
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 2;
+        if (args.length > 0) {
+            days = Integer.parseInt(args[0]) + 1;
+        }
+
+        for (int i = 0; i < days; i++) {
+            log("-------- day " + i + " --------", allLogs);
+            log("name, sellIn, quality", allLogs);
+            for (Item item : items) {
+                log(item.toString(), allLogs);
+            }
+            logEmpty(allLogs);
+            app.updateQuality();
+        }
+
+        return allLogs;
+    }
+
+    private static void logEmpty(ArrayList<String> allLogs) {
+        System.out.println();
+        allLogs.add("");
+    }
+
+    private static void log(String message, ArrayList<String> allLogs) {
+//        System.out.println(message);
+        allLogs.add(message);
+    }
+
+}
