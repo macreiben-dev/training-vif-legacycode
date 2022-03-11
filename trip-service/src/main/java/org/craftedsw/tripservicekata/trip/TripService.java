2bb0bde8404e69fd788f7a69c4b5150e97f76735
MacReiben <macreiben@gmail.com>
2022-03-11 12:06:29 +0100

    invert if on logged user test, early return pattern on findTripList

--- trip-service/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ trip-service/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,23 +11,24 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
+
 		User loggedUser = getLoggedUser();
 
 		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
+
+		if (loggedUser == null) {
 			throw new UserNotLoggedInException();
 		}
+
+		for (User friend : user.getFriends()) {
+			if (friend.equals(loggedUser)) {
+				return findTripsByUser(user);
+			}
+		}
+//		if (isFriend) {
+//			tripList = findTripsByUser(user);
+//		}
+		return tripList;
 	}
 
 	protected List<Trip> findTripsByUser(User user) {
