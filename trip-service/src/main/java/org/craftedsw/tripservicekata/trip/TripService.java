b9124dc7bd2f3cf721208a501ee57a853d167522
MacReiben <macreiben@gmail.com>
2022-03-11 10:57:27 +0100

    initial steps to recover legacy code.

--- trip-service/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ trip-service/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,8 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedUser();
+
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -28,5 +29,8 @@ public class TripService {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+	protected User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
 }
--- trip-service/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ trip-service/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,10 +1,60 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
 	@Test
-    public void SomeTest() {
+    public void when_logged_user_is_null_then_fail() {
+        TripService target = new TestableTripServiceWithNullLoggedUser();
 
+        Assertions.assertThrows(
+            UserNotLoggedInException.class,
+            () -> target.getTripsByUser(new User()));
+    }
+
+    @Test
+    public void when_given_user_is_null_then_fail() {
+        TripService target = new TestableTripServiceWithNotNullLoggedUser();
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> target.getTripsByUser(null));
+    }
+
+    public class TestableTripService extends TripService {
+        private User _loggedUser;
+
+        public TestableTripService WithNullLoggedUser() {
+            _loggedUser = null;
+
+            return this;
+        }
+
+        public TestableTripService WithNotNullLoggedUser() {
+            _loggedUser = new User();
+
+            return this;
+        }
+
+        @Override
+        protected User getLoggedUser() {
+            return _loggedUser;
+        }
+    }
+
+    public class TestableTripServiceWithNotNullLoggedUser extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return new User();
+        }
+    }
+
+    public class TestableTripServiceWithNullLoggedUser extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
     }
 }
